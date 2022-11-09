737b2871710542f0d1a6d7c44d2ea5fee5fffddc
Zhenёq <Zheneq@users.noreply.github.com>
2022-11-09 22:45:51 +0300

    Groups

---
+++ LobbyServer2/LobbyServer/Group/GroupInfo.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using EvoS.Framework.Constants.Enums;
+using log4net;
+
+namespace CentralServer.LobbyServer.Group
+{
+    public class GroupInfo
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(GroupInfo));
+        
+        public readonly long GroupId;
+        public readonly List<long> Members;
+        public long Leader { get; private set; }
+        
+        public GameType GameType;
+        public ushort SubTypeMask;
+        public BotDifficulty AllyDifficulty;
+        public BotDifficulty EnemyDifficulty;
+        public long CreateGameTimestamp;
+
+        public GroupInfo(long id)
+        {
+            this.GroupId = id;
+            this.Members = new List<long>();
+            this.Leader = -1;
+        }
+
+        public bool IsEmpty()
+        {
+            return Members.Count == 0;
+        }
+
+        public bool IsLeader(long accountId)
+        {
+            return accountId == Leader;
+        }
+
+        public void AddPlayer(long accountId)
+        {
+            if (Members.Contains(accountId))
+            {
+                log.Error($"Attempted to re-add player {accountId} to group {GroupId}");
+            }
+            Members.Add(accountId);
+            if (Members.Count == 1)
+            {
+                Leader = accountId;
+            }
+        }
+
+        public void RemovePlayer(long accountId)
+        {
+            if (!Members.Remove(accountId))
+            {
+                log.Error($"Attempted to remove player {accountId} from group {GroupId} when they weren't there");
+                return;
+            }
+            if (Members.Count == 0)
+            {
+                Leader = -1;
+            }
+            else if (Leader == accountId)
+            {
+                Leader = Members[0];
+            }
+        }
+    }
+}
--- LobbyServer2/LobbyServer/Group/GroupManager.cs
+++ LobbyServer2/LobbyServer/Group/GroupManager.cs
@@ -1,30 +1,158 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using CentralServer.LobbyServer.Session;
+using EvoS.Framework.Constants.Enums;
 using EvoS.Framework.DataAccess;
 using EvoS.Framework.Network.Static;
+using log4net;
 
 namespace CentralServer.LobbyServer.Group
 {
     class GroupManager
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(GroupManager));
+        
+        private static readonly Dictionary<long, GroupInfo> ActiveGroups = new Dictionary<long, GroupInfo>();
+        private static readonly Dictionary<long, long> PlayerToGroup = new Dictionary<long, long>();
+        private static long _nextGroupId = 0;
+        private static readonly object _lock = new object();
+
+        public static void CreateGroup(
+            long leader,
+            GameType gameType,
+            ushort subTypeMask,
+            BotDifficulty allyDifficulty,
+            BotDifficulty enemyDifficulty)
+        {
+            LeaveGroup(leader, false);
+            long groupId;
+            lock (_lock)
+            {
+                groupId = _nextGroupId++;
+                ActiveGroups.Add(
+                    groupId,
+                    new GroupInfo(groupId)
+                    {
+                        GameType = gameType,
+                        SubTypeMask = subTypeMask,
+                        AllyDifficulty = allyDifficulty,
+                        EnemyDifficulty = enemyDifficulty,
+                        CreateGameTimestamp = DateTimeOffset.Now.ToUnixTimeSeconds(),
+                    });
+            }
+            JoinGroup(groupId, leader);
+        }
+
+        public static void LeaveGroup(long accountId, bool warnIfNotInAGroup = true)
+        {
+            lock (_lock)
+            {
+                if (PlayerToGroup.TryGetValue(accountId, out long groupId))
+                {
+                    GroupInfo groupInfo = ActiveGroups[groupId];
+                    groupInfo.RemovePlayer(accountId);
+                    PlayerToGroup.Remove(accountId);
+                    log.Info($"Removed {accountId} from group {groupId}");
+                    if (groupInfo.IsEmpty())
+                    {
+                        ActiveGroups.Remove(groupId);
+                        log.Info($"Group {groupId} disbanded");
+                    }
+                }
+                else if (warnIfNotInAGroup)
+                {
+                    log.Warn($"Player {accountId} attempted to leave a group while not being in one");
+                }
+            }
+        }
+
+        public static void JoinGroup(long groupId, long accountId)
+        {
+            LeaveGroup(accountId, false);
+            lock (_lock)
+            {
+                if (ActiveGroups.TryGetValue(groupId, out GroupInfo groupInfo))
+                {
+                    groupInfo.AddPlayer(accountId);
+                    PlayerToGroup.Add(accountId, groupId);
+                    log.Info($"Added {accountId} to group {groupId}");
+                }
+                else
+                {
+                    log.Error($"Player {accountId} attempted to join a non-existing group {groupId}");
+                }
+            }
+        }
+
+        private static UpdateGroupMemberData GetMemberData(GroupInfo groupInfo, long accountId)
+        {
+            PersistedAccountData account = DB.Get().AccountDao.GetAccount(accountId);
+            CharacterComponent characterComponent = account.CharacterData[account.AccountComponent.LastCharacter].CharacterComponent;
+
+            return new UpdateGroupMemberData()
+            {
+                MemberDisplayName = account.Handle,
+                MemberHandle = account.Handle,
+                HasFullAccess = true,
+                IsLeader = groupInfo.IsLeader(account.AccountId),
+                IsReady = false, // TODO
+                IsInGame = false, // TODO
+                CreateGameTimestamp = groupInfo.CreateGameTimestamp,
+                AccountID = account.AccountId,
+                MemberDisplayCharacter = account.AccountComponent.LastCharacter,
+                VisualData = new GroupMemberVisualData
+                {
+                    VisualInfo = characterComponent.LastSkin,
+                    ForegroundBannerID = account.AccountComponent.SelectedForegroundBannerID,
+                    BackgroundBannerID = account.AccountComponent.SelectedBackgroundBannerID,
+                    TitleID = account.AccountComponent.SelectedTitleID,
+                    RibbonID = account.AccountComponent.SelectedRibbonID,
+                },
+                PenaltyTimeout = DateTime.MinValue,
+                GameLeavingPoints = 0
+            };
+        }
+        
         public static LobbyPlayerGroupInfo GetGroupInfo(long accountId)
         {
-            // TODO
-            LobbyServerProtocol client = SessionManager.GetClientConnection(accountId);
-
-            LobbyPlayerGroupInfo groupInfo = new LobbyPlayerGroupInfo()
+            GroupInfo groupInfo = null;
+            lock (_lock)
             {
-                SelectedQueueType = client.SelectedGameType,
-                MemberDisplayName = client.UserName,
-                //InAGroup = false,
-                //IsLeader = true,
-                Members = new List<UpdateGroupMemberData>(),
-            };
-            
+                if (PlayerToGroup.TryGetValue(accountId, out long groupId))
+                {
+                    groupInfo = ActiveGroups[groupId];
+                }
+            }
+
             PersistedAccountData account = DB.Get().AccountDao.GetAccount(accountId);
-            groupInfo.SetCharacterInfo(LobbyCharacterInfo.Of(account.CharacterData[account.AccountComponent.LastCharacter]));
-            
-            return groupInfo;
+            LobbyServerProtocol client = SessionManager.GetClientConnection(accountId);
+            LobbyPlayerGroupInfo response;
+            if (groupInfo == null)
+            {
+                response = new LobbyPlayerGroupInfo
+                {
+                    SelectedQueueType = client.SelectedGameType,
+                    MemberDisplayName = account.Handle,
+                    InAGroup = false,
+                    IsLeader = true,
+                    Members = new List<UpdateGroupMemberData>(),
+                };
+            }
+            else
+            {
+                response = new LobbyPlayerGroupInfo
+                {
+                    SelectedQueueType = groupInfo.GameType,
+                    SubTypeMask = groupInfo.SubTypeMask,
+                    MemberDisplayName = account.Handle,
+                    InAGroup = true,
+                    IsLeader = groupInfo.IsLeader(account.AccountId),
+                    Members = groupInfo.Members.Select(id => GetMemberData(groupInfo, id)).ToList()
+                };
+            }
+            response.SetCharacterInfo(LobbyCharacterInfo.Of(account.CharacterData[account.AccountComponent.LastCharacter]));
+            return response;
         }
     }
 }
