1e53506efc438cb293eeb09b98ea2b7e41f00d91
Zhenёq <Zheneq@users.noreply.github.com>
2023-01-23 21:26:03 +0300

    Revert "Reconnection quick fix"
    This reverts commit 8cbcab059167ae7a87c14a800381f2500b63b775.

--- LobbyServer2/LobbyServer/LobbyServerProtocol.cs
+++ LobbyServer2/LobbyServer/LobbyServerProtocol.cs
@@ -196,13 +196,11 @@ namespace CentralServer.LobbyServer
                     log.Info(string.Format(Messages.LoginSuccess, this.UserName));
                     RegisterGameClientResponse response = new RegisterGameClientResponse
                     {
-                        // Note: If we send AuthInfo back, it will override the one currently set up on the client
-                        // which will cause issues if the client attempts to reconnect at some point.
-                        // AuthInfo = new AuthInfo()
-                        // {
-                        //     AccountId = AccountId,
-                        //     Handle = playerInfo.Handle
-                        // },
+                        AuthInfo = new AuthInfo()
+                        {
+                            AccountId = AccountId,
+                            Handle = playerInfo.Handle
+                        },
                         SessionInfo = SessionManager.GetSessionInfo(request.AuthInfo.AccountId),
                         ResponseId = request.RequestId
                     };
