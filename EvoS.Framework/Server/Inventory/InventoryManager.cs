1ad6a348460cd21692df4f8ce77bb99e0a500b46
mts508 <35640466+mts508@users.noreply.github.com>
2023-02-01 09:22:42 -0300

    Some emojis crash the game (#25)

--- EvoS.Framework/Server/Inventory/InventoryManager.cs
+++ EvoS.Framework/Server/Inventory/InventoryManager.cs
@@ -13,7 +13,7 @@ namespace EvoS.DirectoryServer.Inventory
 
         public static List<int> GetUnlockedEmojiIDs(long accountId)
         {
-            return new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25, 21, 22, 23, 24, 26, 27, 31, 30, 28, 29, 32, 33, 34, 35, 38, 37, 36, 39, 22, 23, 24, 41, 42, 43, 44, 45 };
+            return new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25, 21, 26, 27, 31, 30, 28, 29, 32, 33, 34, 35, 38, 37, 36, 39, 22, 23, 24, 41, 42, 43, 44, 45 };
         }
 
         public static List<int> GetUnlockedLoadingScreenBackgroundIds(long accountId)
