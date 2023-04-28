3052a97e373cf4a9ac8ad91d65f4f18e8c428004
zheneq <zheneq@users.noreply.github.com>
2023-04-28 14:32:48 +0200

    Serializer fix

--- EvoS.Framework/Misc/QuestItemReward.cs
+++ EvoS.Framework/Misc/QuestItemReward.cs
@@ -1,8 +1,10 @@
+using System;
 using EvoS.Framework.Network;
 
 namespace EvoS.Framework.Misc
 {
     [EvosMessage(421)]
+    [Serializable]
     public class QuestItemReward
     {
         public int ItemTemplateId;
