ce0bce698fd4c443cba5d2238fa47e9084e543e2
zheneq <zheneq@users.noreply.github.com>
2023-06-22 01:56:31 +0300

    React image paths

--- evos.admin/src/lib/Resources.ts
+++ evos.admin/src/lib/Resources.ts
@@ -6,15 +6,15 @@ export enum BannerType {
 }
 
 export function playerBanner(type: BannerType, id: number) {
-    return `/banners/${type}/${id}.png`;
+    return `/img/banners/${type}/${id}.png`;
 }
 
 export function characterIcon(characterType: CharacterType) {
-    return `/characters/icons/${characterType}.png`;
+    return `/img/characters/icons/${characterType}.png`;
 }
 
 export function mapMiniPic(map: MapType) {
-    return `/maps/mini/${map}.png`;
+    return `/img/maps/mini/${map}.png`;
 }
 
 
