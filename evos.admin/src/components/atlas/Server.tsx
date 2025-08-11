fa5c3f6726d99c58ffb856718e09c28476d9bc88
zheneq <zheneq@users.noreply.github.com>
2025-08-11 22:13:23 +0400

    reformat code

--- evos.admin/src/components/atlas/Server.tsx
+++ evos.admin/src/components/atlas/Server.tsx
@@ -1,27 +1,26 @@
-import { useState } from "react";
+import {useState} from "react";
 import {
-  GameData,
-  PlayerData,
-  ServerData,
-  shutdownServer,
+    GameData,
+    PlayerData,
+    ServerData,
+    shutdownServer,
 } from "../../lib/Evos";
 import {
-  Box,
-  Button,
-  SelectChangeEvent,
-  Tooltip,
-  Typography,
+    Box,
+    Button,
+    Tooltip,
+    Typography,
 } from "@mui/material";
 import Game from "./Game";
-import { useAuthHeader } from "react-auth-kit";
+import {useAuthHeader} from "react-auth-kit";
 import BaseDialog from "../generic/BaseDialog";
-import { processError } from "../../lib/Error";
-import { useNavigate } from "react-router-dom";
+import {processError} from "../../lib/Error";
+import {useNavigate} from "react-router-dom";
 
 interface Props {
-  info: ServerData;
-  game?: GameData;
-  playerData: Map<number, PlayerData>;
+    info: ServerData;
+    game?: GameData;
+    playerData: Map<number, PlayerData>;
 }
 
 function buildTitle(info: ServerData, game?: GameData) {
@@ -33,61 +32,61 @@ function buildTitle(info: ServerData, game?: GameData) {
     return info.name + suffix;
 }
 
-export default function Server({ info, game, playerData }: Props) {
-  const [msg, setMsg] = useState<string>();
-  const [processing, setProcessing] = useState<boolean>();
-  const [showConfirmDialog, setShowConfirmDialog] = useState<boolean>(false);
-  const authHeader = useAuthHeader();
-  const navigate = useNavigate();
+export default function Server({info, game, playerData}: Props) {
+    const [msg, setMsg] = useState<string>();
+    const [processing, setProcessing] = useState<boolean>();
+    const [showConfirmDialog, setShowConfirmDialog] = useState<boolean>(false);
+    const authHeader = useAuthHeader();
+    const navigate = useNavigate();
 
-  const shutdownServerHandler = () => {
-    setProcessing(true);
+    const shutdownServerHandler = () => {
+        setProcessing(true);
 
-    const abort = new AbortController();
-    shutdownServer(abort, authHeader(), info.id)
-      .then(() => setMsg("Server shutdown initiated."))
-      .catch((e) => processError(e, (err) => setMsg(err.text), navigate))
+        const abort = new AbortController();
+        shutdownServer(abort, authHeader(), info.id)
+            .then(() => setMsg("Server shutdown initiated."))
+            .catch((e) => processError(e, (err) => setMsg(err.text), navigate))
 
-    return () => abort.abort();
-  };
+        return () => abort.abort();
+    };
 
-  return (
-    <>
-      <Box
-        display="flex"
-        alignItems="center"
-        gap={2}
-        justifyContent="center"
-        sx={{ p: 2 }}
-      >
-        <BaseDialog title={msg} onDismiss={() => setMsg(undefined)} />
-        <BaseDialog 
-          title="Confirm Server Shutdown"
-          content={`Are you sure you want to shutdown ${info.name}?`}
-          onDismiss={() => setShowConfirmDialog(false)}
-          onAccept={() => {
-            setShowConfirmDialog(false);
-            shutdownServerHandler();
-          }}
-          acceptText="Stop game"
-          dismissText="Cancel"
-          props={{ open: showConfirmDialog }}
-        />
-        <Tooltip arrow title={info.id}>
-          <Typography variant="h3">{buildTitle(info, game)}</Typography>
-        </Tooltip>
-        {game && info.name !== "Custom game" && (
-            <Button
-              disabled={processing}
-              variant="contained"
-              color="primary"
-              onClick={() => setShowConfirmDialog(true)}
+    return (
+        <>
+            <Box
+                display="flex"
+                alignItems="center"
+                gap={2}
+                justifyContent="center"
+                sx={{p: 2}}
             >
-              Stop game
-            </Button>
-        )}
-      </Box>
-      {game && <Game info={game} playerData={playerData} expanded />}
-    </>
-  );
+                <BaseDialog title={msg} onDismiss={() => setMsg(undefined)}/>
+                <BaseDialog
+                    title="Confirm Server Shutdown"
+                    content={`Are you sure you want to shutdown ${info.name}?`}
+                    onDismiss={() => setShowConfirmDialog(false)}
+                    onAccept={() => {
+                        setShowConfirmDialog(false);
+                        shutdownServerHandler();
+                    }}
+                    acceptText="Stop game"
+                    dismissText="Cancel"
+                    props={{open: showConfirmDialog}}
+                />
+                <Tooltip arrow title={info.id}>
+                    <Typography variant="h3">{buildTitle(info, game)}</Typography>
+                </Tooltip>
+                {game && info.name !== "Custom game" && (
+                    <Button
+                        disabled={processing}
+                        variant="contained"
+                        color="primary"
+                        onClick={() => setShowConfirmDialog(true)}
+                    >
+                        Stop game
+                    </Button>
+                )}
+            </Box>
+            {game && <Game info={game} playerData={playerData} expanded/>}
+        </>
+    );
 }
